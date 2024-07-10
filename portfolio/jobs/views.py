dc97c857e4f594db02cbc75e7002ec7948a1b2b0
sathiriyanboopath13 <sathiriyanmech13@gmail.com>
2024-07-10 11:05:18 +0530

    add portfolio

---
+++ portfolio/jobs/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render,get_object_or_404
+from .models import Job
+
+# Create your views here.
+
+def homepage(request):
+    jobs = Job.objects
+    return render(request,'jobs/home.html',{'jobs':jobs})
+
+def details(request,job_id):
+   job= get_object_or_404(Job,pk=job_id)
+   return render(request, 'jobs/details.html',{'job':job})
